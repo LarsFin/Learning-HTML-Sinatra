8e00ea31f56add9746b7116a4665d862841bfab6
Keran <kerbraich@gmail.com>
2017-12-04 16:48:37 +0000

    add param

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @name = ['Tom', 'Jerry', 'Viking', 'Ted'].sample
+  erb(:catload)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:catload)
 end
