875e80e17545a1a0087f6683cfd465172fd7c29a
Lars <larsfinlay@googlemail.com>
2017-12-04 17:40:15 +0000

    Using forms

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require 'sinatra'
 
-get '/random-cat' do
-  @name = ['Tom', 'Jerry', 'Viking', 'Ted'].sample
-  erb(:catload)
-end
+# get '/random-cat' do
+#   @name = ['Tom', 'Jerry', 'Viking', 'Ted'].sample
+#   erb(:catload)
+# end
 
 get '/named-cat' do
   p params
   @name = params[:name]
   erb(:catload)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:catupload)
 end
